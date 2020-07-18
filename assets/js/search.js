ea7b0f1415e8143164958b5dbb3e4228843a329a
Subham Karmakar <subhamk.subs@gmail.com>
2020-07-18 20:02:03 +0530

    Added the Search List JS

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,19 +1,19 @@
 
 function SearchBlogs() {
-    var input, input2, table, row, data, i, txtValue;
+    var input, input2, sheet, elements, data, i, txtValue;
     input = document.getElementById("searchQue");
     input2 = input.value.toLowerCase();
-    table = document.getElementById("myBlogs");
-    row = table.getElementsByTagName("tr");
+    sheet = document.getElementById("myCheatSheets");
+    elements = sheet.getElementsByTagName("li");
 
-    for (i = 0; i < row.length; i++) {
-        data = row[i].getElementsByTagName("td")[0];
+    for (i = 0; i < elements.length; i++) {
+        data = elements[i].getElementsByTagName("p")[0];
         if (data) {
             txtValue = data.textContent || data.innerText || data.innerHTML;
             if (txtValue.toLowerCase().indexOf(input2) > -1) {
-                row[i].style.display = "";
+                elements[i].style.display = "";
             } else {
-                row[i].style.display = "none";
+                elements[i].style.display = "none";
             }
         }
     }
